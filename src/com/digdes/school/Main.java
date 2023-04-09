877bb2fa8dcbd774298d76493ac4571f68b90fbc
p.manilov <pavman0123456789@gmail.com>
2023-04-10 00:14:00 +0300

    Main commit

--- src/com/digdes/school/Main.java
+++ src/com/digdes/school/Main.java
@@ -8,12 +8,5 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws RequestException {
-        JavaSchoolStarter table = new JavaSchoolStarter();
-        table.execute("INSERT VALUES 'id'=1 , 'lastname'='Pablo', 'age'=22, 'active' = False , 'cost'=1.2");
-        table.execute("Update values 'id'=2 , 'lastname'='Like' where 'lastname' like 'Pablo'");
-        System.out.println(table.execute("Select "));
-        System.out.println(table.execute("DELETE"));
-        System.out.println(table.execute("Select "));
-        /*System.out.println(table.convertSetExpression("'lastName' = 'Pablo' AND 'id' = 1 Or  'lastname'  like  '%or'"));*/
     }
 }
